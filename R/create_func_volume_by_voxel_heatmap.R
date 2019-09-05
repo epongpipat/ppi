44101615283920cccefe8bb21b7fe4371a95cec8
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2019-09-05 11:17:19 -0500

    Create create_func_volume_by_voxel_heatmap.R

---
+++ R/create_func_volume_by_voxel_heatmap.R
@@ -0,0 +1,26 @@
+create_func_volume_by_voxel_heatmeap <- function(in_file, out_file) {
+
+  packages <- c("tictoc", "oro.nifti", "furrr", "purrr", "tidyr", "ggplot2")
+  xfun::pkg_attach(packages, install = T, message = F)
+  plan(multiprocess)
+  
+  df <- readNIfTI(in_file)
+  df_flat <- flatten_dimension_all(df@.Data)
+  
+  df_flat <- df_flat %>%
+    group_by(i,j,k) %>%
+    nest() %>%
+    mutate(voxel = 1:nrow(.)) %>%
+    unnest()
+  
+  fig <- ggplot(df_flat, aes(l, voxel, fill = data)) + 
+    geom_raster() +
+    theme_minimal() +
+    scale_fill_distiller(palette = "RdBu") +
+    labs(x = "\nVolume",
+         y = "Voxel\n",
+         fill = "BOLD")
+  
+  ggsave(out_file, fig, width = 6, height = 4)
+
+}
