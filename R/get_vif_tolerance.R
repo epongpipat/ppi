1dca649c88355acf3c2dfc564c99d99783997170
Ekarin Pongpipat <epongpipat@gmail.com>
2020-02-05 18:32:25 -0600

    udpate funcs

--- R/get_vif_tolerance.R
+++ R/get_vif_tolerance.R
@@ -4,21 +4,33 @@
 #'
 #' @return
 #' @export
-#'
+#' @import car
 #' @examples
-get_vif_tolerance <- function(predictors) {
-  predictors <- as.data.frame(predictors)
-  r_sq <- NULL
-  for (i in 1:ncol(predictors)) {
-    temp_model <- lm(predictors[,i] ~ as.matrix(predictors[,-i]))
-    temp_r_sq <- glance(temp_model)$r.squared
-    r_sq <- c(r_sq, temp_r_sq)
+get_vif_tolerance <- function(model) {
+
+  predictors <- model.matrix(model) %>%
+    as.data.frame() %>%
+    select(-"(Intercept)")
+
+  n_predictors <- ncol(predictors)
+
+  if (n_predictors == 1) {
+    data <- NA
+  } else {
+
+    r_sq <- NULL
+
+    for (i in 1:ncol(predictors)) {
+      temp_model <- lm(predictors[,i] ~ as.matrix(predictors[,-i]))
+      temp_r_sq <- glance(temp_model)$r.squared
+      r_sq <- c(r_sq, temp_r_sq)
+    }
+    data <- tibble(term = colnames(predictors),
+                   r_sq = r_sq) %>%
+      rowwise() %>%
+      mutate(vif = 1 / (1 - r_sq),
+             tolerance = 1 / vif) %>%
+      ungroup()
   }
-  data <- tibble(term = colnames(predictors),
-                      r_sq = r_sq) %>%
-    rowwise() %>%
-    mutate(vif = 1 / (1 - r_sq),
-           tolerance = 1 / vif) %>%
-    ungroup()
-  return(as.data.frame(data))
+  return(data)
 }
--- R/model_glm_roi2roi.R
+++ R/model_glm_roi2roi.R
@@ -7,30 +7,42 @@
 #' @export
 #' @import dplyr furrr
 #' @examples
-model_glm_roi2roi <- function(target_roi, design_matrix) {
+model_glm_roi2roi <- function(target_roi, design_matrix, formula = NULL) {
 
-  y <- as.matrix(target_roi)
-  n_target <- ncol(target_roi)
-
-  design_matrix <- X
-
-  if (is.matrix(design_matrix) & length(dim(design_matrix)) == 2) {
-    design_matrix_3d <- array(NA, c(dim(design_matrix), 1))
-    design_matrix_3d[,,1] <- as.matrix(design_matrix)
-  } else if (is.list(design_matrix)) {
-    dim_3d <- c(dim(design_matrix[[1]]), length(design_matrix))
-    design_matrix_3d <- array(NA, dim_3d)
-    for (i in 1:length(design_matrix)) {
-      design_matrix_3d[,,i] <- as.matrix(design_matrix[[i]])
+  # convert target_roi to list ----
+  y <- list()
+  if (is.list(target_roi)) {
+    y <- target_roi
+    for (i in 1:length(y)) {
+      colnames(y[[i]]) <- "target_roi"
     }
-
-  } else if (length(design_matrix) == 3) {
-    design_matrix_3d <- design_matrix
+  } else if (is.matrix(target_roi) || is.data.frame(target_roi) || ncol(as.matrix(design_matrix)) == 1) {
+    target_roi <- target_roi %>% as.matrix()
+    for (i in 1:ncol(target_roi)) {
+      y[[i]] <- target_roi[,i] %>% as.matrix()
+      colnames(y[[i]]) <- "target_roi"
+    }
   } else {
-    stop("design matrix input must be 2D, 3D, or a list")
+    stop("target_roi must be an array, 2D, or a list")
   }
 
-  n_seed <- dim(design_matrix_3d)[3]
+  # convert design_matrix to list ----
+  X <- list()
+  if (is.list(design_matrix) & !is.data.frame(design_matrix)) {
+    X <- design_matrix
+  } else if ((is.matrix(design_matrix) || is.data.frame(design_matrix)) & length(dim(design_matrix)) == 2) {
+    X[[1]] <- design_matrix %>% as.matrix()
+  } else if ( length(dim(design_matrix)) == 3 ) {
+    for (i in 1:(dim(design_matrix)[3])) {
+      X[[i]] <- design_matrix[,,i] %>% as.matrix()
+    }
+  } else {
+    stop("design_matrix must be 2D, 3D, or a list")
+  }
+
+  # number of seeds and targets ----
+  n_target <- length(y)
+  n_seed <- length(X)
 
   # set up data ----
   data <- tibble(n_target = 1:n_target) %>%
@@ -38,20 +50,28 @@ model_glm_roi2roi <- function(target_roi, design_matrix) {
     unnest() %>%
     mutate(n_model = row_number()) %>%
     unnest() %>%
-    mutate(y = future_map(n_target, function(x) as.matrix(target_roi[, x])),
-           X = future_map(n_seed, function(x) as.matrix(design_matrix_3d[,,x])))
+    mutate(y = future_map(n_target, function(x) y[[x]] %>% as.matrix()),
+           X = future_map(n_seed, function(x) X[[x]] %>% as.matrix()),
+           data = future_map2(y, X, function(y, X) cbind(y, X) %>% as.data.frame()))
 
-  # perform data analysis ----
-  # and obtain useful statistics
-  data_analysis <- data %>%
-    mutate(model = future_map2(y, X, function(y, X) lm(y ~ X)),
-           vif_tol = future_map(X, get_vif_tolerance),
+  #perform data analysis ----
+  if (is.null(formula)) {
+    data_analysis <- data %>%
+      mutate(model = future_map2(y, X, function(y, X) lm(y ~ X)))
+  } else {
+    data_analysis <- data %>%
+      mutate(model = future_map(data, function(x) lm(as.formula(paste0("target_roi ~ ", formula)), x)))
+  }
+
+  # extract useful information ----
+  data_extracts <- data_analysis %>%
+    mutate(vif_tol = future_map(model, get_vif_tolerance),
            residual = future_map(model, "residuals"),
-           residual = future_map(residual, as.data.frame),
+           residual = future_map(residual, function(x) as.data.frame(x, col.names = "residual")),
            tidy = future_map(model, tidy),
            tidy = future_map2(tidy, X, function(x, y) bootPermBroom::tidy_lm_add_r_squared(x, nrow(y)) %>% as.data.frame()),
            glance = future_map(model, function(x) glance(x) %>% as.data.frame()))
 
-  return(data_analysis)
+  return(data_extracts)
 
 }
