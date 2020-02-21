6dee2bc285cc16fd63ab0d81f0dfcd0e3a9c5ba2
Ekarin Pongpipat <epongpipat@gmail.com>
2020-02-21 17:03:11 -0600

    update create data wrangling func

--- R/create_data_wrangling_report.R
+++ R/create_data_wrangling_report.R
@@ -12,11 +12,12 @@
 #' @import rmarkdown
 #' @examples
 create_data_wrangling_report <- function(data_wrangling, out_dir = NULL, out_prefix = NULL, out_suffix = NULL, quiet = TRUE) {
+  input_file <- system.file("rmd", "report_data_wrangling.Rmd", package = "ppi")
   if (is.null(out_dir)) {
     out_dir <- getwd()
   } else if (!dir.exists(out_dir)) {
     dir.create(out_dir, recursive = T)
   }
   out_path <- paste0(out_dir, "/", out_prefix, "data_wrangling_report", out_suffix, ".html")
-  rmarkdown::render(system.file("rmd", "report_data_wrangling.Rmd", package = "ppi"), output_file = out_path, clean = T, params = list(data_wrangling = data_wrangling), quiet = quiet)
+  rmarkdown::render(input = input_file, output_file = out_path, clean = T, params = list(data_wrangling = data_wrangling), quiet = quiet)
 }
